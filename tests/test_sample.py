be24b15a210725b70f8df15486d8ca855a9e1ac1
Otavio Jacobi <otaviojacobi@gmail.com>
2020-11-11 15:23:54 -0200

    action breaks without flake8

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,8 +1,8 @@
-import pytest
 import requests
 
 
 API_ENDPOINT = 'http://ec2-35-180-41-68.eu-west-3.compute.amazonaws.com:8080/api/intent'
+
 
 def test_capital_case():
     assert 'Semaphore' == 'Semaphore'
@@ -10,6 +10,7 @@ def test_capital_case():
 
 def test_capital_case_diff():
     assert 'Semaphore' != 'semaphore'
+
 
 def test_api_up():
     r = requests.get(API_ENDPOINT + '?sentence=hello')
