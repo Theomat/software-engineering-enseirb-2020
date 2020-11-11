51e850b36731985de72921937cdfd6e49fc90c97
Otavio Jacobi <otaviojacobi@gmail.com>
2020-11-11 13:38:24 -0200

    debugging test

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -10,5 +10,5 @@ def test_capital_case_diff():
     assert 'Semaphore' != 'semaphore'
 
 def test_api_up():
-    r = requests.get('http://localhost:8080/api/intent?sentence=trouve des toilettes')
+    r = requests.get('http://ec2-35-180-41-68.eu-west-3.compute.amazonaws.com:8080/api/intent?sentence=trouve des toilettes')
     assert r.status_code == 200
