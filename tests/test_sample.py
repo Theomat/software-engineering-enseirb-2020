9c3634939dbb8816b819aad023462865d195bf7f
Otavio Jacobi <otaviojacobi@gmail.com>
2020-11-11 12:52:57 -0200

    test if service is up

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,4 +1,6 @@
 import pytest
+import requests
+
 
 def test_capital_case():
     assert 'Semaphore' == 'Semaphore'
@@ -6,3 +8,7 @@ def test_capital_case():
 
 def test_capital_case_diff():
     assert 'Semaphore' != 'semaphore'
+
+def test_api_up():
+    r = requests.get('http://localhost:8080/api/intent?sentence=trouve des toilettes')
+    assert r.status_code == 200
