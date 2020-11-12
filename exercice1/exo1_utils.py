d6e0ad7b99f8b464c2ce9038fad9b821c811ee0d
Theomat <theomatricon@gmail.com>
2020-11-12 13:02:59 +0100

    Improved documentation

--- exercice1/exo1_utils.py
+++ exercice1/exo1_utils.py
@@ -1,15 +1,25 @@
 import requests
 
-from typing import List
+from typing import List, Dict
 import json
 
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 
 
-def get_intent_probabilities(sentence: str, endpoint: str = 'http://localhost:8080/api/intent') -> str:
+def get_intent_probabilities(sentence: str, endpoint: str = 'http://localhost:8080/api/intent') -> Dict[str, float]:
     """
     Return the probabilities for each intent for the specified sentence.
+    It raises a RuntimeError if the status code to the GET request is anything other than HTTP_OK.
+
+    Parameters
+    -----------
+    - **sentence**: the sentence whose intent is to be guessed
+    - **endpoint**: (*optional*) the intent route to the endpoint, default to localhost route on port 8080.
+
+    Return
+    -----------
+    A dictionnary where the keys are the intent-class and the values are the probabilities.
     """
     r = requests.get(endpoint + '?sentence=' + sentence)
     if r.status_code == 200:
@@ -22,6 +32,15 @@ def get_intent_probabilities(sentence: str, endpoint: str = 'http://localhost:80
 def get_most_probable_intent(sentence: str, endpoint: str = 'http://localhost:8080/api/intent') -> str:
     """
     Return the intent with the highest probability for the specified sentence.
+
+    Parameters
+    -----------
+    - **sentence**: the sentence whose intent is to be guessed
+    - **endpoint**: (*optional*) the intent route to the endpoint, default to localhost route on port 8080.
+
+    Return
+    -----------
+    The intent with the highest probability accroding to the endpoint.
     """
     result = get_intent_probabilities(sentence, endpoint)
     return max(result, key=result.get)
@@ -29,7 +48,15 @@ def get_most_probable_intent(sentence: str, endpoint: str = 'http://localhost:80
 
 def load_json_data(filename: str) -> dict:
     """
-    Load the content of the json file as a dict.
+    Load the content of the JSON file as a dict.
+
+    Parameters
+    -----------
+    - **filename**: the path to the JSON file to be loaded
+
+    Return
+    -----------
+    A dictionnary containing the contents of the specified file.
     """
     f = open(filename, 'r')
     file_content = json.load(f)
@@ -39,31 +66,68 @@ def load_json_data(filename: str) -> dict:
 
 def get_all_intents(endpoint: str = 'http://localhost:8080/api/intent') -> List[str]:
     """
-    Send a sample sentence to get a list of all possible intents.
+    Send a sentence to get a list of all possible intents.
+
+    Parameters
+    -----------
+    - **endpoint**: (*optional*) the intent route to the endpoint, default to localhost route on port 8080.
+
+    Return
+    -----------
+    The list of all intent class.
     """
     r = requests.get(endpoint + '?sentence=im a teapot')  # classify anything just to have the labels
     return list(r.json().keys())
 
 
-def get_raw_training_data(path: str = './training_set.json'):
+def get_raw_training_data(path: str = './training_set.json') -> List[Dict[str, str]]:
     """
-    Load the raw training set.
+    Load the content of the raw training data.
+
+    Parameters
+    -----------
+    - **path**: (*optional*) the path to the dataset
+
+    Return
+    -----------
+    A List of dictionnaries.
+    Each dictionnary has two keys 'intent' and 'sentence'.
+    'intent' has a str value which is the intent-class.
+    'sentence' has a str value which is the sentence.
     """
     return load_json_data(path)
 
 
-def get_raw_testing_data(path: str = './testing_set.json'):
+def get_raw_testing_data(path: str = './testing_set.json') -> List[Dict[str, str]]:
     """
-    Load the raw testing set.
+    Load the content of the raw testing data.
+
+    Parameters
+    -----------
+    - **path**: (*optional*) the path to the dataset
+
+    Return
+    -----------
+    A List of dictionnaries.
+    Each dictionnary has two keys 'intent' and 'sentence'.
+    'intent' has a str value which is the intent-class.
+    'sentence' has a str value which is the sentence.
     """
     return load_json_data(path)
 
 
-def score(raw_data: dict, endpoint: str = 'http://localhost:8080/api/intent') -> dict:
+def score(raw_data: List[Dict[str, str]], endpoint: str = 'http://localhost:8080/api/intent') -> dict:
     """
-    Compute the scores based on the given raw JSON dataset.
+    Compute the scores based on the given raw dataset.
 
-    The output dict contains two keys:
+    Parameters
+    -----------
+    - **raw_data**: the dataset to compute metrics on.
+    - **endpoint**: (*optional*) the intent route to the endpoint, default to localhost route on port 8080.
+
+    Return
+    ----------
+    The output dictionnary contains two keys:
         - 'report': which is a classification report given by scikit learn
         - 'cm': which is a confusion matrix not normalized
     """
@@ -76,9 +140,19 @@ def score(raw_data: dict, endpoint: str = 'http://localhost:8080/api/intent') ->
     }
 
 
-def limit_intent(raw_data: dict, intent: str, max_qty: int) -> dict:
+def limit_intent(raw_data: List[Dict[str, str]], intent: str, max_qty: int) -> List[Dict[str, str]]:
     """
-    Limit the number of specified intent in the data set.
+    Limit the number of examples of the specified intent-class in the dataset to the maximum quantity defined.
+
+    Parameters
+    -----------
+    - **raw_data**: the dataset to be changed.
+    - **intent**: the intent class to be limited
+    - **max_qty**: the maximum numbers of example of that class allowed.
+
+    Return
+    ----------
+    A new dataset but with the limited amount of examples for the specified intent class.
     """
     current = 0
     output = []
